85a335dcec61acffdebacfad87e7b838221435da
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-12-13 12:58:02 +0500

    Date fixed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import moment from "moment";
+
+
 
 import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
@@ -12,23 +15,13 @@ import {
   demoChannelUrl,
   demoChannelTitle,
 } from "../utils/constant";
-import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoCard = ({ vedio: {id: { videoId },snippet,},}) => {
+  // Convert the published date string to a moment object
+  const publishedMoment = moment(snippet?.publishedAt);
 
-//   const{videoDetail,setVideoDetail}=useState([])
-  
-  
-
-//   useEffect(() => {
-//     fetchFromAPI(
-//       `videos?part=contentDetails%2Csnippet%2Cstatistics&id=${videoId}`
-//     ).then((data)=>setVideoDetail(data));
-   
-//   }, [videoId])
-  
-// console.log(videoDetail);
-
+  // Calculate the difference between the published date and the current date
+  const timeAgo = publishedMoment.fromNow();
 
   return (
     <Card
@@ -73,48 +66,12 @@ const VideoCard = ({ vedio: {id: { videoId },snippet,},}) => {
             {snippet?.channelTitle || demoChannelTitle}
             <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
           </Typography>
-          <Typography variant="subtitle2" color="gray">
-            12 views {snippet?.publishedAt} ago
+          <Typography variant="subtitle2" color="gray" display='flex' justifyContent='start' gap={2} marginTop={0.4}>
+            <span> 0 views</span> <span>{timeAgo}</span>
           </Typography>
         </Link>
       </CardContent>
     </Card>
-    // <Card
-    //   sx={{
-    //     width: { xs: "100%", sm: "358px", md: "320px" },
-    //     boxShadow: "none",
-    //     borderRadius: 0,
-    //   }}
-    // >
-    //   <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
-    //     <CardMedia
-    //       image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
-    //       alt={snippet?.title}
-    //       sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
-    //     />
-    //   </Link>
-    //   <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
-    //     <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-    //       <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-    //         {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-    //       </Typography>
-    //     </Link>
-    //     <Link
-    //       to={
-    //         snippet?.channelId
-    //           ? `/channel/${snippet?.channelId}`
-    //           : demoChannelUrl
-    //       }
-    //     >
-    //       <Typography variant="subtitle2" color="gray">
-    //         {snippet?.channelTitle || demoChannelTitle}
-    //         <CheckCircle
-    //           sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
-    //         />
-    //       </Typography>
-    //     </Link>
-    //   </CardContent>
-    // </Card>
   );
 };
 
